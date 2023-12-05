af8a3bbaaf4bb6a78216dc632c154509ea18a427
Sarthakxtiwari <143182039+Sarthakxtiwari@users.noreply.github.com>
2023-12-05 10:02:44 +0530

    not imp

--- mainpagefrontend/app/page.js
+++ mainpagefrontend/app/page.js
@@ -143,8 +143,6 @@ function MainPage() {
       time: "2 days",
     },
     {
-      userName: "user 1234",
-      userProfileUrl: "https://imgs.search.brave.com/6yz2O2VMpgQz_QxPjezntKKQCz7JzLGM10nxx8rjT9s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2hvbWUvYmx1cmJz/L3Zpc3VhbHMud2Vi/cA",
       id: "slfwoli4342",
       userId: "sifi423idsf",
       userName: "Axios",
